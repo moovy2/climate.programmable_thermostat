64cef4c6239ff04f6e627cf9c63dc83d32d74672
MapoDan <42698485+MapoDan@users.noreply.github.com>
2026-03-13 12:51:54 +0100

    Update const.py

--- custom_components/programmable_thermostat/const.py
+++ custom_components/programmable_thermostat/const.py
@@ -3,7 +3,7 @@ from homeassistant.components.climate import HVACMode
 from homeassistant.const import Platform
 
 #Generic
-VERSION = '8.6'
+VERSION = '9.0beta'
 DOMAIN = 'programmable_thermostat'
 PLATFORM = [Platform.CLIMATE]
 ISSUE_URL = 'https://github.com/custom-components/climate.programmable_thermostat/issues'
